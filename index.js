f43596c5af1905a5e8567d4d20e3a4599ddeda1b
Naman <namankumar62039@gmail.com>
2024-02-11 01:17:44 +0530

    connected with dataBase

--- index.js
+++ index.js
@@ -9,3 +9,22 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+// dataBase connection with mongoDb
+
+mongoose.connect("mongodb+srv://namankumar62039:Namankumar62039%K@cluster0.m8xmhmh.mongodb.net/e-commers")
+
+// API creation
+
+app.get("/", (req, res) => {
+    res.send("Express app is Running")
+})
+
+app.listen(PORT, (error) => {
+    if (!error) {
+        console.log(`Server Running On Port: ${PORT}`)
+    }
+    else{
+        console.log(`Error: ${error}`)
+    }
+})
