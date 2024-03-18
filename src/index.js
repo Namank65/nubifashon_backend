baad2626ed20578d577a2c4d758aaf9ce319c366
Naman <namankumar62039@gmail.com>
2024-03-19 00:51:06 +0530

    made the api to upload the images through multer middleware

--- src/index.js
+++ src/index.js
@@ -31,6 +31,15 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
+app.use("/images", express.static('upload/images'))
+
+app.post("/upload", upload.single('product'), (req,res) => {
+    res.json({
+        success: 1,
+        image_url: `http://localhost:${port}/images${req.file.filename}`
+    })
+})
+
 app.listen(PORT, (error) => {
     if (!error) {
         console.log(`Server Running On Port: ${PORT}`)
