4d8d4e7bcf8f85df645945bc264f206a9ee99859
Naman <namankumar62039@gmail.com>
2024-03-22 01:09:09 +0530

    created a product model to store in dataBase

---
+++ src/models/Product.model.js
@@ -0,0 +1,42 @@
+import mongoose, { Schema } from 'mongoose';
+
+const productSchema = new Schema({
+    id: {
+        type: Number,
+        require: true
+    },
+    name: {
+        type: String,
+        require: true
+    },
+    image: {
+        type: String,
+        require: true
+    },
+    category: {
+        type: String,
+        require: true
+    },
+    newPrice: {
+        type: Number,
+        require: true
+    },
+    oldPrice: {
+        type: Number,
+        require: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    avilable: {
+        type: Boolean,
+        default: true
+    }
+},
+    {
+        timestamps: true
+    }
+);
+
+export const Product = mongoose.model("Product", productSchema);
