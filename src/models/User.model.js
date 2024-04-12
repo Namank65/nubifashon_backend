e6757c9f20d6b39cb67b070fe3ad8705ed98bbec
Naman <namankumar62039@gmail.com>
2024-04-13 01:39:01 +0530

    not getting email buged fix

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -12,6 +12,13 @@ const userSchema = new Schema({
         trim: true,
         index: true
     },
+    email: {
+        type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+    },
     password:
     {
         type: String,
