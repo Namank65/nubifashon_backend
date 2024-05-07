c05a5fad26f915865e8bfc092b5caf905bf04827
Naman <namankumar62039@gmail.com>
2024-05-08 02:37:20 +0530

    solving multer

--- src/MiddleWare/Multer.middleWare.js
+++ src/MiddleWare/Multer.middleWare.js
@@ -2,14 +2,12 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads/')
+        cb(null,  path.resolve(__dirname, './images'))
     },
     filename: function (req, file, cb) {
         cb(null, file.fieldname)
     }
 });
-
-//"./upload/images"
 
 export const upload = multer({
     storage
