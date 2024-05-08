2fb9b141b1804edab9ba1ace7c66640d379a9f88
Naman <namankumar62039@gmail.com>
2024-05-08 12:05:23 +0530

    solveing the multer err

--- src/MiddleWare/Multer.middleWare.js
+++ src/MiddleWare/Multer.middleWare.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null,  path.resolve(__dirname, '/images'))
+        cb(null,  '../upload/images')
     },
     filename: function (req, file, cb) {
         cb(null, file.fieldname)
