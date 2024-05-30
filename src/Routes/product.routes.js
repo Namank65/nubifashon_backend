248ffe6543608ab5196e57339b37ee0fc60f2dd0
Naman <namankumar62039@gmail.com>
2024-05-31 01:31:09 +0530

    bug Fixing

--- src/Routes/product.routes.js
+++ src/Routes/product.routes.js
@@ -1,7 +1,6 @@
 import { Router } from "express";
-import { addProduct, addToCart, allProducts, newCollection, popularInWomen, productUpload, removeProduct } from "../Controllers/product.controller.js";
+import { addProduct, addToCart, allProducts, newCollection, popularInWomen, productUpload, removeFromCart, removeProduct } from "../Controllers/product.controller.js";
 import { upload } from "../MiddleWare/Multer.middleWare.js";
-import veryfyJwt from "../MiddleWare/auth.middleWare.js";
 import productAuth from "../MiddleWare/productAuth.middleware.js";
 
 
@@ -17,4 +16,5 @@ router.route("/allProducts").get(allProducts);
 router.route("/newCollection").get(newCollection);
 router.route("/popularInWomen").get(popularInWomen);
 router.route("/addToCart").post(productAuth, addToCart);
+router.route("/removeFromCart").post(productAuth, removeFromCart);
 export default router;
