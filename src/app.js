08350487b72a467cc20c1a6e9fcf689c32f775b7
Naman <namankumar62039@gmail.com>
2024-06-01 01:21:14 +0530

    corse change

--- src/app.js
+++ src/app.js
@@ -4,11 +4,36 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-}))
+// app.use(cors({
+//     origin: process.env.CORS_ORIGIN,
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true
+// }))
+
+// Split the CORS_ORIGINS environment variable into an array
+const allowedOrigins = process.env.CORS_ORIGINS.split(',');
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Allow requests with no origin (e.g., mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    
+    // Check if the origin is in the allowedOrigins array
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
+
+
+
+
 
 app.use(express.json({limit: "10000kb"}));
 app.use(express.urlencoded({extended: true, limit: "10000kb"}));
