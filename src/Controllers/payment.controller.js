fabd3596da6c1bb7ce2da3b495291f9954099588
Naman <namankumar62039@gmail.com>
2024-06-06 00:38:29 +0530

    fixed the payment checkout controller

--- src/Controllers/payment.controller.js
+++ src/Controllers/payment.controller.js
@@ -5,10 +5,11 @@ import apiResponse from "../utils/apiResponce.js";
 
 export const checkout = asyncHandler(async(req, res) => {
     const options = {
-        amount: Number(req.body * 100),
+        amount: Number(req.body.amount * 100),
         currency: "INR",
       };
       
       const order = await instance.orders.create(options);
+      console.log(order)
       return res.status(200).json(new apiResponse(201, order , "Payment Successfull"));
 })
