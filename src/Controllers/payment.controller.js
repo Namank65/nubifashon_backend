f81028803e79968f8b1e720bd21de94bbc325038
Naman <namankumar62039@gmail.com>
2024-06-05 20:08:55 +0530

    sending responce or order details

--- src/Controllers/payment.controller.js
+++ src/Controllers/payment.controller.js
@@ -5,12 +5,10 @@ import apiResponse from "../utils/apiResponce.js";
 
 export const checkout = asyncHandler(async(req, res) => {
     const options = {
-        amount: 50000,  // amount in the smallest currency unit
+        amount: Number(req.body * 100),
         currency: "INR",
       };
-      const order = await instance.orders.create(options)
-
-      console.log(order);
-
-      return res.status(200).json(new apiResponse(201, {success: true} , "Payment Successfull"))
+      
+      const order = await instance.orders.create(options);
+      return res.status(200).json(new apiResponse(201, {success: true, order} , "Payment Successfull"));
 })
