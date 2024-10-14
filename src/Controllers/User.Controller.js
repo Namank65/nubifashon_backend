da3f8db9f610bc62d91c3d66409962e5ae350a76
Souamya <127884520+Namank65@users.noreply.github.com>
2024-10-14 20:03:05 +0530

    Update User.Controller.js
    commiting for consistency

--- src/Controllers/User.Controller.js
+++ src/Controllers/User.Controller.js
@@ -231,6 +231,7 @@ const myUserProfile = asyncHandeler(async(req, res) => {
     // Adding another comment for consistency
     // Adding another comment for consistency
     // Adding last comment for consistency
+    // Adding almost last for comment for consisitency because of fiver
     res.status(200).json(new apiResponse(201, {user: req.user}, "working fine"))
 })
 
