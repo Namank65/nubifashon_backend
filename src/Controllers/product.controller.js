fd53b9b15683b97b4bc10efe1b2ab5e35946c9b8
souamya <namankumar62039@gmail.com>
2024-10-21 00:43:45 +0530

    fixing the bug

--- src/Controllers/product.controller.js
+++ src/Controllers/product.controller.js
@@ -25,7 +25,7 @@ const productUpload = asyncHandler(async (req, res) => {
 
 })
 
-// this api is under construction
+// this api is under construction and will complet it soon
 
 // const newProduct1 = asyncHandler(async (req, res) => {
 //     const { name, images, category, newPrice, oldPrice, size, stock } = req.body;
