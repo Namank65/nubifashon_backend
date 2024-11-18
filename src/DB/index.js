81fd3aca71babc28d2a3e79d1d371e7881d43698
souamya <namankumar62039@gmail.com>
2024-11-18 22:38:51 +0530

    made the allignment more better

--- src/DB/index.js
+++ src/DB/index.js
@@ -2,13 +2,17 @@ import mongoose from "mongoose";
 import { Db_Name } from "../Constants.js";
 
 const ConnectDB = async () => {
-    try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${Db_Name}`);
-        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`)
-    } catch (error) {
-        console.log(`MongoDB Connection Error ${error}`)
-        process.exit(1)
-    }
-}
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URL}/${Db_Name}`
+    );
+    console.log(
+      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log(`MongoDB Connection Error ${error}`);
+    process.exit(1);
+  }
+};
 
 export default ConnectDB;
