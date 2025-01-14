fea39016e74c703fc8f53e8414dcd422e5f7f777
souamya <namankumar62039@gmail.com>
2025-01-15 00:35:48 +0530

    Fixed payment sucess issue2

--- src/Controllers/payment.controller.js
+++ src/Controllers/payment.controller.js
@@ -38,7 +38,7 @@ export const paymentVerification = asyncHandler(async (req, res) => {
     });
 
     res.redirect(
-      `https://nubifashon-backend.onrender.com/api/v1/paymentsuccess?refrence=${razorpay_payment_id}`
+      `https://nubifashon-backend.onrender.com/api/v1/payment/paymentVerification?refrence=${razorpay_payment_id}`
     );
   } else {
     return res
