cbc5b1f428dbab657cfd8797e45a241891489bb3
souamya <namankumar62039@gmail.com>
2025-02-04 00:24:34 +0530

    fixing payment issue redirection bug day4 try 12

--- src/Controllers/payment.controller.js
+++ src/Controllers/payment.controller.js
@@ -37,8 +37,7 @@ export const paymentVerification = asyncHandler(async (req, res) => {
       razorpay_signature,
     });
 
-   return res.redirect(`http://localhost:3000/paymentsuccess?refrence=${razorpay_payment_id}`);
-    // res.json({ redirectUrl: `https://nubifashon.web.app/paymentsuccess?refrence=${razorpay_payment_id}` });
+   return res.redirect(`paymentsuccess?refrence=${razorpay_payment_id}`);
 
   } else {
     return res
