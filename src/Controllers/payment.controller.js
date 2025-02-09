2f7f7b357475b3b7d3fc9035d24b992eee323e8b
souamya <namankumar62039@gmail.com>
2025-02-10 01:55:48 +0530

    trying to fix the  payment bug

--- src/Controllers/payment.controller.js
+++ src/Controllers/payment.controller.js
@@ -37,7 +37,7 @@ export const paymentVerification = asyncHandler(async (req, res) => {
       razorpay_signature,
     });
 
-   return res.redirect(`http://localhost:3000/paymentsuccess?refrence=${razorpay_payment_id}`);
+   return res.redirect(`https://nubifashon.web.app/paymentsuccess?refrence=${razorpay_payment_id}`);
 
   } else {
     return res
