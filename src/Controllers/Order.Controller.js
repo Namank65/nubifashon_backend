f35692601ca279df3f0e52d389eb30e72ed8648a
Souamya <127884520+Namank65@users.noreply.github.com>
2025-03-01 12:50:01 +0530

    Update Order.Controller.js
    cometteng for consistency

--- src/Controllers/Order.Controller.js
+++ src/Controllers/Order.Controller.js
@@ -32,8 +32,8 @@ const newOrder = asyncHandler(async (req, res) => {
     // const objectId = new mongoose.Types.ObjectId(ProductId)
     
     // const order = await Order.findOneAndDelete({ ProductId: objectId});
-    // const order = await Order.findOne({orderItems: {$elemMatch: {_id: new mongoose.Types.ObjectId('67bb747bd5eff42becd7e600')}}});
-    // console.log(order);
+     const order = await Order.findOne({orderItems: {$elemMatch: {_id: new mongoose.Types.ObjectId('67bb747bd5eff42becd7e600')}}});
+     console.log(order);
     
      if (!order) throw new apiError(400, "Couldn't Found The Order");
     
@@ -57,7 +57,7 @@ const allOrders = asyncHandler(async (req, res) => {
     const order = await Order.find({}).populate('user', 'userName')
     console.log(order);
     
-    if(!order) throw new apiError(401, "Something Went Wrong While Getting All Orders ");
+    if(!order) throw new apiError(401, "Something Went Wrong While Getting All Orders");
   
     return res
       .status(200)
